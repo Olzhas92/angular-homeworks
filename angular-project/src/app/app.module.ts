c23f0d94dbfd4ed4eb6c838175a625975b343d4e
olzhaskhamzin <olzhas.khamzin@gmail.com>
2023-04-19 00:50:21 +0600

    input added

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DeliveryComponent } from './components/delivery/delivery.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DeliveryComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, DeliveryComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- angular-project/src/app/components/delivery/delivery.component.ts
+++ angular-project/src/app/components/delivery/delivery.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/types/data';
 
 @Component({
   selector: 'app-delivery',
   templateUrl: './delivery.component.html',
-  styleUrls: ['./delivery.component.scss']
+  styleUrls: ['./delivery.component.scss'],
 })
-export class DeliveryComponent {
+export class DeliveryComponent implements OnInit {
+  orders: Order[] = [];
+  newGoodsName: string;
+  newGoodsPrice: number;
 
+  ngOnInit(): void {}
+
+  onSubmit() {
+    if (!this.newGoodsName && this.newGoodsPrice) {
+      alert('Add Goods');
+      return;
+    }
+
+    const newOrder: Order = {
+      name: this.newGoodsName,
+      price: this.newGoodsPrice,
+    };
+
+    this.orders.push(newOrder);
+  }
 }
